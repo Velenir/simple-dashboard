ad53696d008b994abc5321f360ae1902e2d716aa
Velenir <Velenir@users.noreply.github.com>
2017-07-30 21:39:21 +0300

    Add Placeholder to use in place of not implemented widgets

---
+++ src/components/Placeholder/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const style = {
+  width: '100%',
+  height: '90%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: '#2882a0',
+  backgroundColor: 'rgba(171, 153, 153, 0.24)',
+  padding: 10,
+  backgroundClip: 'content-box',
+  border: '1px solid #dcdcdc'
+};
+
+const Placeholder = () => (
+  <div style={style}>
+    Placeholder
+  </div>
+);
+
+export default Placeholder;
