1669a17adfe612074242fbcd8e2b3b5956ed5b38
Velenir <Velenir@users.noreply.github.com>
2017-07-30 19:08:20 +0300

    Add a generic Modal

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './Modal.scss';
+
+class Modal extends Component {
+  render() {
+    const { title, closeButton, children, aside } = this.props;
+
+    return (
+      <div className='modal'>
+        <div className='modal__overlay' />
+        <div className='modal__body'>
+          <div className='modal__header'>
+            <div className='modal__header__title'>{title}</div>
+            {closeButton && <button className='modal__button modal__button--close' />}
+          </div>
+          <div className='modal__aside'>
+            {typeof aside === 'function' ? aside() : aside}
+          </div>
+          <div className='modal__main'>
+            {typeof children === 'function' ? children() : children}
+          </div>
+          <div className='modal__footer'>
+            <button className='modal__button modal__button--cancel' />
+            <button className='modal__button modal__button--save' />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Modal;
